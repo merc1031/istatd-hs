5d07f16dc8baf78743add0ee7f6aef3bcc903e82
Leonidas Loucas <lloucas@imvu.com>
2016-10-20 09:26:30 -0700

    not working yet

--- src/Istatd/TypeSet.hs
+++ src/Istatd/TypeSet.hs
@@ -5,7 +5,9 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 {-# OPTIONS_GHC -fno-warn-redundant-constraints #-}
@@ -15,6 +17,8 @@ module Istatd.TypeSet
 , Nat (..)
 , InSet
 , InList
+--, InList2
+--, NotInList
 , SetToCompound
 , ToCompound
 , SetToCompoundAdd
@@ -24,15 +28,23 @@ module Istatd.TypeSet
 , SetInsert
 , ListInsert
 , UniqueInsert
+, (:+!)
 , Set
 , FirstIdxList
 , SetFromCompound
 , FromCompound
+, IsSubSet
+, IsSubList
 , gconstrn
 , gconstr
 , ShouldHandle (..)
 , uhhh
 , shower
+, inList
+, isSubSet
+, isSubList
+, isSubSetC
+, isSubListC
 )
 where
 
@@ -55,6 +67,12 @@ type family (a :: Bool) :|| (b :: Bool) where
   'True :|| _     = 'True
   _     :|| 'True = 'True
   _     :|| _     = 'False
+--
+-- | Type level `&&`
+type family (a :: Bool) :&& (b :: Bool) where
+  'True :&& 'True = 'True
+  _     :&& _     = 'False
+
 
 
 -- | Type level natural numbers
@@ -76,9 +94,9 @@ type family (m :: Nat) :< (n :: Nat) :: Bool where
 type family SetInsert (s :: *) (a :: *) :: * where
   SetInsert (Set rs) a = Set (ListInsert (InList rs a) rs a)
 
-infixr 0 +!
+infixr 0 :+!
 
-type a +! rs = UniqueInsert rs a
+type a :+! rs = UniqueInsert rs a
 
 -- | Type family for inserting non duplicates into type level list
 type UniqueInsert (rs :: [*]) (a :: *) =
@@ -87,17 +105,37 @@ type UniqueInsert (rs :: [*]) (a :: *) =
 -- | Type family for conditional inserting into type level list
 type family ListInsert (b :: Bool) (rs :: [*]) (a :: *) :: [*] where
   ListInsert 'False r a = a ': r
-  ListInsert _      r _ = r
+  ListInsert 'True  r _ = r
+
+-- | Type family for list membership
+type family IsSubSet (s :: *) (sub :: *) :: Bool where
+  IsSubSet (Set rs) (Set subrs) = IsSubList rs subrs
+
+type family IsSubList (rs :: [*]) (subrs :: [*]) :: Bool where
+  IsSubList rs '[] = 'True
+  IsSubList rs (sr ': srs) = (InList rs sr) :&& (IsSubList rs srs)
+
 
 -- | Type family for list membership
 type family InSet (s :: *) (a :: *) :: Bool where
   InSet (Set rs) a = InList rs a
 
 -- | Type family for list membership
-type family InList (rs :: [*]) (a :: *) :: Bool where
-  InList '[]       x = 'False
+type family InList (rs :: [k]) (a :: k) :: Bool where
+  InList '[] x = 'False
   InList (x ': ys) x = 'True
-  InList (y ': ys) x = 'False :|| (InList ys x)
+  InList (y ': ys) x = InList ys x
+
+--type family NotInList (a :: *) (rs :: [*]) :: Bool where
+--  NotInList x (x ': ys) = 'False
+--  NotInList x (y ': ys) = NotInList x ys
+--  NotInList x '[] = 'True
+
+--type family InList2 (a :: *) (rs :: [*]) :: Bool where
+--  InList2 x '[] = 'False
+--  InList2 x (x ': ys) = 'True
+--  InList2 x (y ': ys) = InList2 x ys
+
 
 -- | Type family for finding index in type level list
 type family FirstIdxList (rs :: [*]) (a :: *) :: Nat where
@@ -157,11 +195,29 @@ type family ToCompound (rs :: [*]) :: * where
 type family SetFromCompound (a :: *) :: * where
   SetFromCompound a = Set (FromCompound a)
 
-type family FromCompound (a :: *) :: [*] where
-  FromCompound (Sum4 a b c d) = a +! b +! c +! d +! '[]
-  FromCompound (Sum3 a b c) = a +! b +! c +! '[]
-  FromCompound (Sum2 a b) = a +! b +! '[]
-  FromCompound a = a +! '[]
+type family FromCompound (a :: k) :: [k] where
+  FromCompound (Sum4 a b c d) = a :+! b :+! c :+! d :+! '[]
+  FromCompound (Sum3 a b c) = a :+! b :+! c :+! '[]
+  FromCompound (Sum2 a b) = a :+! b :+! '[]
+  FromCompound a = a :+! '[]
+
+
+--class Conversions rs srs where
+--  conv :: Proxy rs -> rs -> srs
+----
+----class Tester rs srs where
+----
+----instance (InList2 a '[b,a] ~ 'True) => Tester (Sum2 a b) a where
+--
+--instance ('True ~ IsSubSet (SetFromCompound (Sum2 a b)) (SetFromCompound a), Data a, Data b) => Conversions (Sum2 a b) (a) where
+--  conv _ dat =
+--    let wtf = fromJust $ head $ gmapQ (\d -> cast d) (dat :: Sum2 a b) :: a
+--    in fromJust $ gconstr wtf :: a
+--
+--instance ('True ~ IsSubSet (SetFromCompound (Sum3 a b c)) (SetFromCompound (Sum2 a b)), Data a, Data b, Data c) => Conversions (Sum3 a b c) (Sum2 a b) where
+--  conv _ dat =
+--    let wtf = fromJust $ head $ gmapQ (\d -> cast d) (dat :: Sum3 a b) :: a
+--    in fromJust $ gconstr wtf :: Sum2 a b
 
 deriving instance (Data a, Data b) => Data (Sum2 a b)
 deriving instance (Typeable a, Typeable b) => Typeable (Sum2 a b)
@@ -210,6 +266,7 @@ uhhh :: forall m a compoundUs compoundRest
         , Data a
         , Show a
         , Show compoundRest
+--        , Conversions compoundUs compoundRest
         )
      => Proxy compoundRest
      -> compoundUs
@@ -221,7 +278,27 @@ uhhh _ dat a =
        Just v -> case traceShowId $ gconstr v :: Maybe (ShouldHandle a compoundRest) of
                    Just v' -> a v'
                    Nothing -> return ()
-       Nothing -> return () -- is a
+       Nothing -> a $ ShouldntHandle $ (fromJust $ gconstr v :: compoundRest) --conv (Proxy :: Proxy compoundUs) dat
+
+--forT :: ( cs ~ FromCompound compoundRest
+--        )
+--     => Proxy compoundRest
+--     -> compoundUs
+--     -> compoundRest
+--forT _ dat = 
+
+
+
+--  let wtf = head $ gmapQ (\d -> cast d) (dat :: compoundUs) :: Maybe a
+--  in case traceShowId wtf of
+--       Just v -> case traceShowId $ gconstr v :: Maybe (ShouldHandle a compoundRest) of
+--                   Just v' -> a v'
+--                   Nothing -> return ()
+--       Nothing -> let wtf' = head $ gmapQ (\d -> cast d) (dat :: compoundUs) :: Maybe a
+--                  in case traceShowId $ wtf' of
+--                       Just v' -> a v
+--                       Nothing -> return ()
+
 --  let wtf = gconstr (fromJust $ head $ gmapQ (\d -> cast d) (dat :: compoundUs) :: a) :: (Maybe (ShouldHandle a compoundRest))
 --  in case traceShowId wtf of
 --       Just v -> a v
@@ -238,3 +315,37 @@ uhhh _ dat a =
 --gconv :: (Data t1) => t1 -> Maybe a
 --gconv v =
 --  fromConstrB (fromConstr (toConstr (v :: t1))) (toConstr (Just _t :: Maybe a)) :: Maybe a
+inList :: ( InList x y ~ 'True
+          )
+       => Proxy x
+       -> Proxy y
+       -> ()
+inList _ _ = ()
+
+isSubSetC :: ( IsSubSet (SetFromCompound rs) (SetFromCompound srs) ~ 'True
+            )
+         => Proxy rs
+         -> Proxy srs
+         -> ()
+isSubSetC _ _ = ()
+
+isSubSet :: ( IsSubSet (rs) (srs) ~ 'True
+            )
+         => Proxy rs
+         -> Proxy srs
+         -> ()
+isSubSet _ _ = ()
+
+isSubListC :: ( IsSubList (FromCompound rs) (FromCompound srs) ~ 'True
+            )
+         => Proxy rs
+         -> Proxy srs
+         -> ()
+isSubListC _ _ = ()
+
+isSubList :: ( IsSubList (rs) (srs) ~ 'True
+            )
+         => Proxy rs
+         -> Proxy srs
+         -> ()
+isSubList _ _ = ()
